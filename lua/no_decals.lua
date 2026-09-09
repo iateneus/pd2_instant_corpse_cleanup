8ee1967d7c053f6a0be3fe8fe8abd1767ec7dfda
Matheus Islabão <matheus.2011.mw@hotmail.com>
2026-09-09 16:40:41 -0300

    Refactor decal handling in no_decals.lua

--- lua/no_decals.lua
+++ lua/no_decals.lua
@@ -8,37 +8,36 @@ Hooks:PostHook(GamePlayCentralManager, "init", "ICC_NoDecals_Init", function(sel
     ICC:ApplyDecalSettings(self)
 end)
 
+local original_local_flesh = Hooks:GetFunction(GamePlayCentralManager, "play_impact_flesh")
+Hooks:OverrideFunction(GamePlayCentralManager, "play_impact_flesh", function(self, ...)
+    if ICC.settings.remove_blood then return end
+    return original_local_flesh(self, ...)
+end)
 
-local blood_impact = Idstring("effects/payday2/particles/impacts/blood/blood_impact_a")
-local blood_screen = Idstring("effects/particles/character/player/blood_screen")
+local original_bullet_hit = Hooks:GetFunction(GamePlayCentralManager, "_play_bullet_hit")
+Hooks:OverrideFunction(GamePlayCentralManager, "_play_bullet_hit", function(self, params)
+    local unit = params.col_ray.unit
+    if not ICC.settings.remove_blood or not alive(unit) or not unit:in_slot(self._slotmask_flesh) then
+        return original_bullet_hit(self, params)
+    end
+    local count = #self._play_effects
+    original_bullet_hit(self, params)
+    for i = #self._play_effects, count + 1, -1 do
+        table.remove(self._play_effects, i)
+    end
+end)
 
 local original_flesh = Hooks:GetFunction(GamePlayCentralManager, "sync_play_impact_flesh")
 Hooks:OverrideFunction(GamePlayCentralManager, "sync_play_impact_flesh", function(self, from, dir)
-    if not ICC.settings.remove_decals then
+    if not ICC.settings.remove_blood then
         return original_flesh(self, from, dir)
     end
-    self._effect_manager:spawn({
-        effect = blood_impact,
-        position = from,
-        normal = dir
-    })
-
-    local player = managers.player:player_unit()
-    if player and mvector3.distance_sq(from, player:movement():m_head_pos()) < 40000 then
-        self._effect_manager:spawn({
-            effect = blood_screen,
-            position = Vector3(),
-            rotation = Rotation()
-        })
-    end
-
     local sound_source = self:_get_impact_source()
     sound_source:stop()
     sound_source:set_position(from)
     sound_source:set_switch("materials", "flesh")
     sound_source:post_event("bullet_hit")
 end)
-
 
 
 elseif RequiredScript == "lib/managers/explosionmanager" then
